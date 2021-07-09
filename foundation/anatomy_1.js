e718a07c9336ddb9c9a64039cecdbc36367a6fdb
Endleyson Reis Duarte dos Santos <52048005+Endleyson@users.noreply.github.com>
2021-07-09 07:28:53 -0300

    Fundamentos JS

---
+++ foundation/anatomy_1.js
@@ -0,0 +1,20 @@
+function sayHello(){
+    console.log('hello!')
+}
+sayHello()
+
+function sayHelloTo(name){
+    console.log('Hello ' + name)
+}
+sayHelloTo('Duarte')
+
+function hi(){
+    return 'Hi'
+}
+
+console.log(hi())
+
+function hiName(name){
+    return `Hi ${name}`
+}
+console.log(hiName('Zé'))
