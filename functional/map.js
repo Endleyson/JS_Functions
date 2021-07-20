3c870adff5f038060a6e24f00893c55b2896f8fd
Endleyson Reis Duarte dos Santos <52048005+Endleyson@users.noreply.github.com>
2021-07-19 21:07:56 -0300

    map

---
+++ functional/map.js
@@ -0,0 +1,23 @@
+//Map
+const numbers = [1, 2, 3, 4, 5, 6]
+
+const resultDouble = numbers.map(function(el){
+    return el*2
+} )
+const resultTriple = numbers.map(el=>el*3)
+console.log(resultDouble, resultTriple)
+
+const students =[
+    {name: 'Jake', score:6.4},
+    {name: 'Susan', score:8.6},
+    {name: 'Emma', score:9.4},
+    {name: 'Peter', score:9.1},
+]
+
+const getScore = el => el.score
+
+const result = students
+    .map(getScore)
+    .map(Math.ceil)
+
+console.log(students, result)
